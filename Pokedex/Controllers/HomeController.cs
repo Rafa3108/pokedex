93a18303cb2d6ea26f120656f4770b7354452607
Rafa3108 <gigliottirafaella@gmail.com>
2024-03-11 16:41:04 -0300

    pagina 144

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -32,7 +32,32 @@ public class HomeController : Controller
         return View(Pokemons);
     }
 
-
+        public IActionResult Details (int Id)
+        {   
+            List<Pokemon> pokemons = new ();
+            using (StreamReader leitor = new ("Data\\pokemons.json"))
+            {
+                string dados = leitor.ReadToEnd();
+                pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
+            }
+            List<Tipo> tipos = new();
+            using (StreamReader leitor = new ("Data\\tipos.json"))
+            {
+                string dados = leitor.ReadToEnd();
+                tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
+            }
+            DetailsVM details = new()
+            {
+                Tipos = tipos,
+                Atual = pokemons.FirstOrDefault(p => p.Numero == Id),
+                Anterior = pokemons.OrderByDescending(p => p.Numero).FirstOrDefault(p => p.Numero == Id),
+                Proximo = pokemons.OrderBy(p => p.Numero).FirstOrDefault(p => p.Numero == Id),
+            };
+            var pokemon = pokemons
+                .Where (p => p.Numero == Id)
+                .FirstOrDefault();
+            return View (details);
+        }
     public IActionResult Privacy()
     {
         return View();
